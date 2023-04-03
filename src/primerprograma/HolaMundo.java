ddef8f3394a9a24fdc5e09c4258d16a987ee45aa
Luciana <Lucy15269@gmail.com>
2023-04-03 17:38:02 -0300

    primer commit

---
+++ src/primerprograma/HolaMundo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package primerprograma;
+
+/**
+ *
+ * @author Lucy Bianchini
+ */
+public class HolaMundo{
+
+ //la linea 12 muestra el mensaje por pantalla al ejecutar el programa
+    
+    public static void main(String[] args) {
+        
+        System.out.println("Hola mundo");
+    }
+    
+}
